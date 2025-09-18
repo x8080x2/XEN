e3a5d5bbdf0c827dec94e37ca301617479059424
x8080x2 <eu2@eeu.jp>
2025-09-18 10:25:50 +0000

    Restored to '397df91bca1e6b6e8e65ad76d569b37f7796a994'
    Replit-Restored-To: 397df91bca1e6b6e8e65ad76d569b37f7796a994

--- user-package/client/src/App.tsx
+++ user-package/client/src/App.tsx
@@ -1,12 +1,12 @@
-
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import OriginalEmailSender from "@/pages/OriginalEmailSender";
 
-function Router() {
+function RouterComponent() {
   return (
     <Switch>
       <Route path="/" component={OriginalEmailSender} />
@@ -19,7 +19,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Router hook={useHashLocation}>
+          <RouterComponent />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
